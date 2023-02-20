d735e72999e3f8297f9265a83d8f665145722cdc
Ahmad-hallaq <ahmadhallaq02@gmail.com>
2023-02-20 16:05:27 +0300

    added models

--- sab_training_management/base/models.py
+++ sab_training_management/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Admin(models.Model):
+    Admin_id: int
+    Name: models.CharField()
+    Email: models.CharField()
+    Phone_number: models.CharField()
+
+    def __str__(self):
+        return self.Name
+
+
